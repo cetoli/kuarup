b110d1540115beb9f6fd3c00f2a7771efae45419
andremoraes78 <andremoraes78@980eeb38-f416-11dd-aa46-671127399dce>
2009-02-11 19:26:09 +0000

    programa funcionando oo

--- pyniv09/trunk/andre/adivinho.py
+++ pyniv09/trunk/andre/adivinho.py
@@ -42,78 +42,84 @@ o meu palpite é 4
 '''
 #@nonl
 #@-node:carlo.20081219153110.8:Documentação
-#@+node:carlo.20090104111700.4:Participantes
-def apresenta_participantes():
-    return "\nEu sou o Jogo do Advinho, você é o voluntário! \n"
-#@nonl
-#@-node:carlo.20090104111700.4:Participantes
-#@+node:aluno.20090211135634.3:Globais
-resposta = ""
-palpite = 1
-nao_acertou = True
-#@nonl
-#@-node:aluno.20090211135634.3:Globais
-#@+node:carlo.20090104111700.5:Regra do jogo
-def diz_a_regra_do_jogo():    
-    return "Pense um número de 1 a 100 \n" "Diga a se acertei, s para eu subir o palpite e d para eu descer o palpite."
-#@-node:carlo.20090104111700.5:Regra do jogo
-#@+node:carlo.20090104111700.7:Palpite
-def da_um_palpite():
-    #import random
-    #global palpite
+#@+node:aluno.20090211135634.6:Classe Advinhacao
+class Adivinhacao:
+    #@    @+others
+    #@+node:aluno.20090211135634.3:Globais
+    resposta = ""
+    palpite = 1
+    nao_acertou = True
+    #@nonl
+    #@-node:aluno.20090211135634.3:Globais
+    #@+node:carlo.20090104111700.4:Participantes
+    def apresenta_participantes(self):
+        return "\nEu sou o Jogo do Advinho, você é o voluntário! \n"
+    #@nonl
+    #@-node:carlo.20090104111700.4:Participantes
+    #@+node:carlo.20090104111700.5:Regra do jogo
+    def diz_a_regra_do_jogo(self):    
+        return "Pense um número de 1 a 100 \n" "Diga a se acertei, s para eu subir o palpite e d para eu descer o palpite."
+    #@-node:carlo.20090104111700.5:Regra do jogo
+    #@+node:carlo.20090104111700.7:Palpite
+    def da_um_palpite(self):
+        #import random
+        #global palpite
 
-    #palpite = random.randint(1,7)
-    return "\nEu acho que você pensou no número " + str(palpite)
-#@-node:carlo.20090104111700.7:Palpite
-#@+node:aluno.20090206164348.2:Resposta do Voluntário
-def obtem_resposta_voluntario():
-    global resposta 
-    resposta = raw_input ("Diga sua resposta:")
-    return "Você disse: " + resposta
-#@nonl
-#@-node:aluno.20090206164348.2:Resposta do Voluntário
-#@+node:aluno.20090211135634.4:Muda Palpite
-def muda_palpite_pracima():
-    global palpite
-    palpite += 1
+        #palpite = random.randint(1,7)
+        return "\nEu acho que você pensou no número " + str(self.palpite)
+    #@-node:carlo.20090104111700.7:Palpite
+    #@+node:aluno.20090206164348.2:Resposta do Voluntário
+    def obtem_resposta_voluntario(self):
+        #global resposta 
+        self.resposta = raw_input ("Resultado do palpite:")
+        return "Você disse: " + self.resposta
+    #@nonl
+    #@-node:aluno.20090206164348.2:Resposta do Voluntário
+    #@+node:aluno.20090211135634.4:Muda Palpite
+    def muda_palpite_pracima(self):
+        #global palpite
+        self.palpite += 1
 
-def muda_palpite_prabaixo():
-    global palpite
-    palpite -= 1
-#@nonl
-#@-node:aluno.20090211135634.4:Muda Palpite
-#@+node:aluno.20090206164348.3:Pense pra ver se acertou
-def analisa_resposta():
-    global nao_acertou
-    if (resposta == 'a'):
-        nao_acertou = False
-        return ("Acertei sua resposta!");
-    elif (resposta == 's'):
-        muda_palpite_pracima()
-        return ("Deverei subir o meu palpite!");
-    elif (resposta == 'd'):
-        muda_palpite_prabaixo()
-        return ("Deverei descer o palpite");
-    else:
-        return "Comando não reconhecido"
-#@nonl
-#@-node:aluno.20090206164348.3:Pense pra ver se acertou
-#@+node:aluno.20090211135634.5:Tenta varios palpites
-def tenta_varios_palpites():
-    while nao_acertou == True:
-        from time import sleep
-        sleep(2)
+    def muda_palpite_prabaixo(self):
+        #global palpite
+        self.palpite -= 1
+    #@nonl
+    #@-node:aluno.20090211135634.4:Muda Palpite
+    #@+node:aluno.20090206164348.3:Pense pra ver se acertou
+    def analisa_resposta(self):
+        #global nao_acertou
+        if (self.resposta == 'a'):
+            self.nao_acertou = False
+            return ("Acertei sua resposta!");
+        elif (self.resposta == 's'):
+            self.muda_palpite_pracima()
+            return ("Deverei subir o meu palpite!");
+        elif (self.resposta == 'd'):
+            self.muda_palpite_prabaixo()
+            return ("Deverei descer o palpite");
+        else:
+            return "Comando não reconhecido"
+    #@nonl
+    #@-node:aluno.20090206164348.3:Pense pra ver se acertou
+    #@+node:aluno.20090211135634.5:Tenta varios palpites
+    def tenta_varios_palpites(self):
+        while self.nao_acertou == True:
+            from time import sleep
+            sleep(2)
 
-        print da_um_palpite()
-        print obtem_resposta_voluntario()
-        print analisa_resposta()
-#@-node:aluno.20090211135634.5:Tenta varios palpites
-#@+node:aluno.20090211135634.2:Joga o jogo do advinho
-def joga_adivinha_um_numero():
-    print apresenta_participantes()
-    print diz_a_regra_do_jogo()
-    tenta_varios_palpites()
-#@-node:aluno.20090211135634.2:Joga o jogo do advinho
+            print self.da_um_palpite()
+            print self.obtem_resposta_voluntario()
+            print self.analisa_resposta()
+    #@-node:aluno.20090211135634.5:Tenta varios palpites
+    #@+node:aluno.20090211135634.2:Joga o jogo do advinho
+    def joga_adivinha_um_numero(self):
+        print self.apresenta_participantes()
+        print self.diz_a_regra_do_jogo()
+        self.tenta_varios_palpites()
+    #@-node:aluno.20090211135634.2:Joga o jogo do advinho
+    #@-others
+#@nonl
+#@-node:aluno.20090211135634.6:Classe Advinhacao
 #@-others
 #@-node:carlo.20081219153110.7:<< Jogo do Adivinho >>
 #@nl
@@ -123,6 +129,7 @@ def _test():
     doctest.testmod()
 
 if __name__ == "__main__":
-    joga_adivinha_um_numero()
+    minha_adivinhacao = Adivinhacao()
+    minha_adivinhacao.joga_adivinha_um_numero()
 #@-node:carlo.20080914080659.3:@thin adivinho.py
 #@-leo
