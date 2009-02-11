0ab953810fefd36e86aa2035cc50695cedad0734
gabrixbr <gabrixbr@980eeb38-f416-11dd-aa46-671127399dce>
2009-02-11 18:52:04 +0000

    Tenta varios palpites concluido

---
+++ pyniv09/trunk/gabriel/JogoDoAdivinho.rst
@@ -0,0 +1,83 @@
+.. rst3: filename: JogoDoAdivinho.rst
+
+.. footer:: LABASE - NCE
+
+##################
+O Jogo do Adivinho
+##################
+
+:Author: *Seu nome Aqui*
+:Contact: seu email
+:Date: $Date: 2009/02/06  $
+:Status: This is a "work in progress"
+:Revision: $Revision: 1.00 $
+:Home: `LABASE <http://labase.nce.ufrj.br/>`__
+:Copyright: ©2009, `GPL <http://is.gd/3Udt>`__
+
+Introdução
+**********
+
+Programando Jogos
+~~~~~~~~~~~~~~~~~
+
+* Descrever em texto o que se quer
+* Programar para que outros leiam
+* Desenvolver em versões incrementais
+* Testar o que se quer programar
+
+O Jogo do Adivinho
+******************
+
+* Observe uma partida de advinha
+ 
+* Escreva um slide documentando o jogo que advinha
+ 
+* Inicie criando o jogo mais rudimentar - um palpite
+
+* Escreva um teste para saber se o jogo funciona
+
+Descrição do Jogo
+*****************
+
+* o jogo tem um advinho e um voluntário - Participantes
+
+* o advinho pede que se pense um número de 1 a 7 - Regra
+
+* o voluntário pensa em um número - Início
+
+* o advinho dá um palpite - Palpite
+
+* o voluntário diz se o número é maior ou menor que o palpite - Dica
+
+* o diálogo prossegue até que o advinho acerte o palpite - Diálogo
+
+Código do Jogo
+**************
+
+Participantes
+~~~~~~~~~~~~~
+
+**code**:
+
+.. code-block:: Python
+
+	1: print "Eu sou o advinho, você é o voluntário"
+
+Regra do jogo
+~~~~~~~~~~~~~
+
+**code**:
+
+.. code-block:: Python
+
+	1: print "pense um número de 1 a 7"
+
+Palpite
+~~~~~~~
+
+**code**:
+
+.. code-block:: Python
+
+	1: print "o meu palpite é 4"
+
--- pyniv09/trunk/gabriel/adivinho.py
+++ pyniv09/trunk/gabriel/adivinho.py
@@ -42,27 +42,65 @@ o meu palpite é 4
 '''
 #@nonl
 #@-node:carlo.20081219153110.8:Documentação
+#@+node:aluno.20090211141308.9:Glossario
+respostaDoVoluntario = ""
+palpite = 1
+nao_acertou = True
+#@nonl
+#@-node:aluno.20090211141308.9:Glossario
 #@+node:carlo.20090104111700.4:Participantes
-print "Eu sou o advinho, você é o voluntário"
+def apresentaOsParticipantes():
+    print "Eu sou o advinho, você é o voluntário"
 #@nonl
 #@-node:carlo.20090104111700.4:Participantes
 #@+node:carlo.20090104111700.5:Regra do jogo
-print "pense um número de 1 a 7"
+def dizARegraDoJogo():
+    print "pense um número de 1 a 7"
+    print "diga a se acertei, s para eu subir e para eu descer."
 #@nonl
 #@-node:carlo.20090104111700.5:Regra do jogo
 #@+node:carlo.20090104111700.7:Palpite
-print "o meu palpite é 4"
-print "diga a se acertei, s para eu subir, d para descer o palpite"
+def dizOPalpite():
+    print "o meu palpite é " + str(palpite)
+    print "diga a se acertei, s para eu subir, d para descer o palpite"
 #@nonl
 #@-node:carlo.20090104111700.7:Palpite
-#@+node:aluno.20090206161713.2:Escuta o voluntario
-respostaVoluntario = raw_input("diga!")
-print "Voce disse:" + respostaVoluntario
-#@-node:aluno.20090206161713.2:Escuta o voluntario
+#@+node:aluno.20090211141308.16:Tenta Varios Palpites
+def tentaVariosPalpites():
+    while nao_acertou:
+        dizOPalpite()
+        escutaOVoluntario()
+        pensaEVeSeAcertou()
+#@nonl
+#@-node:aluno.20090211141308.16:Tenta Varios Palpites
 #@+node:aluno.20090206161713.3:Pensa e ve se acertou
-if respostaVoluntario =="a" : print "acertei!"
+def pensaEVeSeAcertou():
+    global nao_acertou
+    if respostaVoluntario =="a" : 
+        print "acertei!"
+        nao_acertou=False
+    else:
+        mudaPalpite()
 #@nonl
 #@-node:aluno.20090206161713.3:Pensa e ve se acertou
+#@+node:aluno.20090206161713.2:Escuta o voluntario
+def escutaOVoluntario():
+    global respostaVoluntario
+    respostaVoluntario = raw_input("diga!")
+    print "Voce disse:" + respostaVoluntario
+#@-node:aluno.20090206161713.2:Escuta o voluntario
+#@+node:aluno.20090211141308.15:Muda Palpite
+def mudaPalpite():
+    global palpite
+    palpite = palpite + 1
+#@nonl
+#@-node:aluno.20090211141308.15:Muda Palpite
+#@+node:aluno.20090211141308.2:Joga o Jogo do Adivinho
+def jogaOJogoDoAdivinho():
+    apresentaOsParticipantes()
+    dizARegraDoJogo()
+    tentaVariosPalpites()
+#@-node:aluno.20090211141308.2:Joga o Jogo do Adivinho
 #@-others
 #@-node:carlo.20081219153110.7:<< Jogo do Adivinho >>
 #@nl
@@ -72,6 +110,6 @@ def _test():
     doctest.testmod()
 
 if __name__ == "__main__":
-    _test()
+    jogaOJogoDoAdivinho()
 #@-node:carlo.20080914080659.3:@thin adivinho.py
 #@-leo
