b450f45ff54692151a91dad17213232b3f94c179
andremoraes78 <andremoraes78@980eeb38-f416-11dd-aa46-671127399dce>
2009-02-11 17:18:08 +0000

    Inclusao de arquivos andre

---
+++ pyniv09/trunk/andre/adivinho.py
@@ -0,0 +1,108 @@
+#! /usr/bin/env python
+# -*- coding: UTF8 -*-
+#@+leo-ver=4-thin
+#@+node:carlo.20080914080659.3:@thin adivinho.py
+#@@first
+#@@first
+#@@language python
+#@@tabwidth -4
+#@<< Cabeçalho >>
+#@+node:carlo.20080914080659.5:<< Cabeçalho >>
+"""
+#@+others
+#@+node:carlo.20080914080659.16:@rst-no-head Autor
+##################
+O Jogo do Adivinho
+##################
+
+:Author: *Seu nome Aqui*
+:Contact: seu email
+:Date: $Date: 2009/02/06  $
+:Status: This is a "work in progress"
+:Revision: $Revision: 1.00 $
+:Home: `LABASE <http://labase.nce.ufrj.br/>`__
+:Copyright: ©2009, `GPL <http://is.gd/3Udt>`__
+#@-node:carlo.20080914080659.16:@rst-no-head Autor
+#@-others
+"""
+#@-node:carlo.20080914080659.5:<< Cabeçalho >>
+#@nl
+#@<< Jogo do Adivinho >>
+#@+node:carlo.20081219153110.7:<< Jogo do Adivinho >>
+#@+others
+#@+node:carlo.20081219153110.8:Documentação
+'''
+>>> # Apresenta os participantes
+Eu sou o advinho, você é o voluntário
+>>> # Apresenta as regras
+pense um número de 1 a 7
+>>> # Dá o palpite
+o meu palpite é 4
+>>>
+'''
+#@nonl
+#@-node:carlo.20081219153110.8:Documentação
+#@+node:carlo.20090104111700.4:Participantes
+def apresenta_participantes():
+    return "\nEu sou o Jogo do Advinho, você é o voluntário! \n"
+#@nonl
+#@-node:carlo.20090104111700.4:Participantes
+#@+node:aluno.20090211135634.3:Globais
+resposta = ""
+#@nonl
+#@-node:aluno.20090211135634.3:Globais
+#@+node:carlo.20090104111700.5:Regra do jogo
+def diz_a_regra_do_jogo():    
+    return "Pense um número de 1 a 100 \n" "Diga a se acertei, s para eu subir o palpite e d para eu descer o palpite."
+#@-node:carlo.20090104111700.5:Regra do jogo
+#@+node:carlo.20090104111700.7:Palpite
+def da_um_palpite():
+    import random
+    a = random.randint(1,7)
+    return "\nEu acho que você pensou no número " + str(a)
+#@nonl
+#@-node:carlo.20090104111700.7:Palpite
+#@+node:aluno.20090206164348.2:Resposta do Voluntário
+def obtem_resposta_usuario():
+    global resposta 
+    resposta = raw_input ("Diga sua resposta:")
+    return "Você disse: " + resposta
+#@nonl
+#@-node:aluno.20090206164348.2:Resposta do Voluntário
+#@+node:aluno.20090206164348.3:Pense pra ver se acertou
+def analisa_resposta():
+    if (resposta == 'a'):
+        return ("Acertei sua resposta!");
+    elif (resposta == 's'):
+        return ("Deverei subir o meu palpite!");
+    elif (resposta == 'd'):
+        return ("Deverei descer o palpite");
+    else:
+        return("Não soube identificar sua resposta");
+
+#@-node:aluno.20090206164348.3:Pense pra ver se acertou
+#@+node:aluno.20090211135634.2:Joga o jogo do advinho
+def joga_adivinha_um_numero():
+    print apresenta_participantes()
+    print diz_a_regra_do_jogo()
+
+    from time import sleep
+    sleep(3)
+
+    print da_um_palpite()
+    print obtem_resposta_usuario()
+
+    print analisa_resposta()
+#@-node:aluno.20090211135634.2:Joga o jogo do advinho
+#@-others
+#@-node:carlo.20081219153110.7:<< Jogo do Adivinho >>
+#@nl
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":
+    joga_adivinha_um_numero()
+#@-node:carlo.20080914080659.3:@thin adivinho.py
+#@-leo
