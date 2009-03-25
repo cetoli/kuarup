37f5f702640552a376868c81a906ac499d63eb0b
kuarupp <kuarupp@980eeb38-f416-11dd-aa46-671127399dce>
2009-03-25 07:34:48 +0000

    Trabalho 3 - Movimentos

---
+++ poo09/kuarup/tribos/piraporangatu/Movimentos.py
@@ -0,0 +1,94 @@
+﻿from visual import *
+from piraporangatuOby import *
+from tubarao_tupinamba import *
+from submarino_ticuna import *
+import Image
+
+
+class Cenario:
+
+    
+    def __init__(self):
+        self.scene = display(title = "Movimentos Piraporangatu", width=500, height=500, center= (0,0,0), x=0, y=0)
+        self.scene.x, self.scene.y = 0, 0
+        self.scene.select()
+        self.scene.autoscale = 0
+
+    def principal(self):
+        fish0 = Fish(pos=(15,0,0))
+        fish1 = Fish(pos=(15,4,0))
+        fish2 = Fish(pos=(15,-4,0))
+        fish3 = Fish(pos=(15,0,4))
+        fish4 = Fish(pos=(15,0,-4))
+        submarino = Submarino(escala=4, axis=(1,0,0), pos= (-20,0,0))
+        tubarao = Tubarao(escala=0.5, axis=(-1,0,0), pos=(100,1,0))
+
+        self.frame = 0
+        self.scene.autoscale = 0
+        ## Altera posição da camera
+        self.scene.forward = (-3,-1,-1)
+
+        while 1:
+
+            rate(30)
+            ## Aponta a camera em direção ao cardume
+            self.scene.center = (fish1.esqueleto.pos[0],0,fish1.esqueleto.pos[2])
+
+            if self.frame == 450:
+                ## Altera posição da camera
+                self.scene.forward = (-1,-0.25,0)
+            if self.frame == 600:
+                ## Altera posição da camera
+                self.scene.forward = (3,-1,-1)
+            if 0 <= self.frame <= 900:
+                ## Cardume passeando
+                fish0.swim(direction='forward', speed = 0.1)
+                fish1.swim(direction='forward', speed = 0.1)
+                fish2.swim(direction='forward', speed = 0.1)
+                fish3.swim(direction='forward', speed = 0.1)
+                fish4.swim(direction='forward', speed = 0.1)
+            if self.frame == 700:
+                ## Desvia do tubarão, um morre!
+                fish1.turn(angle=pi/4, axis=(0,0,1))
+                fish2.turn(angle=pi/4, axis=(0,0,-1))
+                fish3.turn(angle=pi/4, axis=(0,-1,0))
+                fish4.turn(angle=pi/4, axis=(0,1,0))
+            if self.frame == 800:
+                ## Retorna a formação de cardume
+                fish1.turn(angle=pi/2, axis=(0,0,-1))
+                fish2.turn(angle=pi/2, axis=(0,0,1))
+                fish3.turn(angle=pi/2, axis=(0,1,0))
+                fish4.turn(angle=pi/2, axis=(0,-1,0))
+            if self.frame == 900:
+                ## Retorna a formação de cardume
+                fish1.turn(angle=pi/4, axis=(0,0,1))
+                fish2.turn(angle=pi/4, axis=(0,0,-1))
+                fish3.turn(angle=pi/4, axis=(0,-1,0))
+                fish4.turn(angle=pi/4, axis=(0,1,0))
+            if 700 < self.frame < 1100:
+                ## Acelera o cardume
+                fish1.swim(direction='forward', speed = 0.2)
+                fish2.swim(direction='forward', speed = 0.2)
+                fish3.swim(direction='forward', speed = 0.2)
+                fish4.swim(direction='forward', speed = 0.2)
+            if 1100 <= self.frame <= 1300:
+                ## Para o cardume e olha para tras
+                fish1.turn(angle=pi/195, axis=(0,1,0))
+                fish2.turn(angle=pi/195, axis=(0,-1,0))
+                fish3.turn(angle=pi/195, axis=(0,-1,0))
+                fish4.turn(angle=pi/195, axis=(0,1,0))
+            if 1300 < self.frame <= 1600:
+                ## Fica parado olhando para tras
+                fish1.stop()
+                fish2.stop()
+                fish3.stop()
+                fish4.stop()
+
+
+            self.frame += 1
+
+            
+
+if __name__ == "__main__":
+    cenario = Cenario()
+    cenario.principal()
---
+++ poo09/kuarup/tribos/piraporangatu/submarino_ticuna.py
@@ -0,0 +1,111 @@
+﻿from visual import *
+
+TITLE = 'Submarino'
+
+class Cenario:
+
+    def __init__(self):
+
+        scene2 = display(title=TITLE, width=150, height=150, center= (0,0,0), x=0, y=0)
+
+        scene2.x, scene2.y =0,0
+
+        scene2.select()
+
+        scene2.autoscale = 1
+
+        self.quadro = 0
+
+    def principal(self):
+
+        Submarino1=Submarino(escala =1, axis=(1,0,1.5), pos= (1,-3,0))
+
+        Submarino2=Submarino(escala =0.2, axis=(1,0,3), pos= (-1,-4,2))
+
+ 
+
+class SerMarinho():
+
+    '''
+
+    Classe base de qualquer ser marinho
+
+    '''
+
+    def __init__(self, escala= 1, **qualquel_outro_parametro):
+
+        "Construtor do ser marinho, definindo um esqueleto(frame) e desenhando"
+
+        self.esqueleto=frame(**qualquel_outro_parametro)
+
+        self.desenha(escala)
+
+    def desenha(self): pass
+
+ 
+
+ # Especialização da classe SerMarinho! Especializamos tanto que virou um Submarino ...
+
+class Submarino (SerMarinho):
+
+    '''
+
+    Esta é o Submarino.
+
+    '''
+
+    def desenha(self, escala=1):
+
+        esc=escala
+
+ 
+
+        # Desenha o Corpo Principal do Submarino
+
+        corpo = ellipsoid(frame = self.esqueleto, pos=(0,0,0), length=10*esc, height=3*esc, width=3*esc, color = color.yellow)
+
+ 
+
+        # Desenha a Ponte ( Passadiço ) do Submarino
+
+        ponte = cylinder(frame = self.esqueleto, pos=(1*esc,0,0),axis=(0,3*esc,0), radius=1*esc, color = color.yellow)
+
+        pontesup = cylinder(frame = self.esqueleto, pos=(1*esc,3*esc,0),axis=(0,0.4*esc,0*esc), radius=.4*esc, color = color.yellow)
+
+        telescopio1 = cylinder(frame = self.esqueleto, pos=(1*esc,3.4*esc,0),axis=(0,0.6*esc,0), radius=.1*esc, color = color.yellow)
+
+        telescopio2 = cylinder(frame = self.esqueleto, pos=(1*esc,3.9*esc,0),axis=(0.2*esc,0,0), radius=.1*esc, color = color.yellow)
+
+        lemedaponte = box(frame = self.esqueleto, pos=(1*esc,2*esc,0), length=1*esc, height=0.05*esc, width=3*esc, color = color.yellow)
+
+ 
+
+        # Desenha o leme de proa do Submarino
+
+        lemedeproa = box(frame = self.esqueleto, pos=(3.5*esc,0*esc,0*esc), length=1*esc, height=0.05*esc, width=3.5*esc, color = color.yellow)
+
+ 
+
+        # Desenha o leme de popa do Submarino
+
+        popa1 = pyramid(frame = self.esqueleto, pos=(-4.5*esc,0,0), size=(1.5*esc,4*esc,0.1*esc), color = color.yellow )
+
+        lemedapopa1 = box(frame = self.esqueleto, pos=(-5*esc,0,0), length=1*esc, height=4*esc, width=0.1*esc, color = color.yellow)
+
+        popa2 = pyramid(frame = self.esqueleto, pos=(-4.5*esc,0,0), size=(1.5*esc,0.1*esc,4*esc), color = color.yellow )
+
+        lemedapopa2 = box(frame = self.esqueleto, pos=(-5*esc,0,0), length=1*esc, height=0.1*esc, width=4*esc, color = color.yellow)
+
+        esteira = helix(frame = self.esqueleto, pos=(-5.8*esc,0,0), axis=(-3*esc,0,0), radius=1*esc)
+
+        esteira2 = helix(frame = self.esqueleto, pos=(-5.8*esc,0,0), axis=(-3*esc,0,0), radius=0.5*esc)
+
+ 
+
+ 
+
+if __name__ == "__main__":
+
+    cenario= Cenario()
+
+    cenario.principal()
