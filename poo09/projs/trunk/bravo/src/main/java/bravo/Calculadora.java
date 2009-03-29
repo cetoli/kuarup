3c22a8bb3c8ba1e7c0e9f582cb331d430881e622
kuarupp <kuarupp@980eeb38-f416-11dd-aa46-671127399dce>
2009-03-29 23:43:48 +0000

    carga inicial 2009

---
+++ poo09/projs/trunk/bravo/src/main/java/bravo/Calculadora.java
@@ -0,0 +1,57 @@
+/*------------------------------------------------------------------------------
+    Copyright  2002-2009        Carlo E. T. Oliveira et all
+    ( see http://labase.nce.ufrj.br/curso/poo/team-list.html )
+
+    This software is licensed as described in the file LICENSE.txt,
+    which you should have received as part of this distribution.
+------------------------------------------------------------------------------*/
+package bravo;
+import labase.poo.ICalculadora;
+
+/**
+ * @todo Escreva a descricao da classe Calculadora aqui.
+ *
+ * @author  (seu nome)  $Author$
+ * @version (versao)    $Revision$ (data)      $Date$
+ * @since   (versao) Descreva aqui as alterações desta versao
+ */
+public class Calculadora implements ICalculadora {
+  /**Acumulador da Caculadora. */
+  private Integer acumulador = new Integer(0);
+  /**Operador da Caculadora. */
+  private Integer operador = new Integer(0);
+
+  /**
+   * Construtor para objetos da classe Calculadora.
+   */
+  public Calculadora() {
+    // inicializa variaveis de instância
+  }
+
+  /**
+   * Entra a tecla um.
+   *
+   * @return  conteudo do operador
+   */
+  public final String entraUm() {
+    return operador.toString();
+  }
+
+  /**
+   * Limpa o acumulador.
+   *
+   * @return  conteudo do acumulador
+   */
+  public final String limpa() {
+    return "0";
+  }
+
+  /**
+   * Entra o comando soma.
+   *
+   * @return  conteudo do acumulador
+   */
+  public final String comandoSoma() {
+    return acumulador.toString();
+  }
+}
---
+++ poo09/projs/trunk/india/src/test/java/india/CalculadoraAcceptanceTest.java
@@ -0,0 +1,41 @@
+/*------------------------------------------------------------------------------
+    Copyright  2002-2009        Carlo E. T. Oliveira et all
+    ( see http://labase.nce.ufrj.br/curso/poo/team-list.html )
+
+    This software is licensed as described in the file LICENSE.txt,
+    which you should have received as part of this distribution.
+------------------------------------------------------------------------------*/
+package india;
+import labase.poo.CalculadoraFactory;
+import labase.poo.ICalculadora;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+/**
+ * Classe que testa o funcionamento da calculadora.
+ *
+ * @author Carlo
+ * @version 1.0
+ * @since 1.0 (07/04/2008):
+ * */
+class CalcFactory  extends CalculadoraFactory{
+  public CalcFactory(){
+    installFactory(this);
+  }
+
+  /**
+   * Cria instancia
+   */
+  public ICalculadora createCalculadora() {
+    return new Calculadora();
+  }
+}
+
+public class CalculadoraAcceptanceTest extends junit.framework.TestCase{
+    
+    public static Test suite() {
+      new CalcFactory();
+      TestSuite suite= new TestSuite(CalculadoraTest.class);
+      suite.addTest(labase.poo.CalculadoraTest.suite());
+      return suite;
+    }
+}
