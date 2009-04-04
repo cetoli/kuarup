18222c9e5e299b21ec686febf3c4d1b31e3b67ee
kuarupp <kuarupp@980eeb38-f416-11dd-aa46-671127399dce>
2009-04-04 03:49:27 +0000

    consertando problemas de formatacao da primeira versao do time charlie

--- poo09/projetos/trunk/charlie/src/main/java/charlie/Calculadora.java
+++ poo09/projetos/trunk/charlie/src/main/java/charlie/Calculadora.java
@@ -1,10 +1,10 @@
 /*------------------------------------------------------------------------------
-    Copyright  2002-2009        Carlo E. T. Oliveira et all
-    ( see http://labase.nce.ufrj.br/curso/poo/team-list.html )
+  Copyright  2002-2009        Carlo E. T. Oliveira et all
+  ( see http://labase.nce.ufrj.br/curso/poo/team-list.html )
 
-    This software is licensed as described in the file LICENSE.txt,
-    which you should have received as part of this distribution.
-------------------------------------------------------------------------------*/
+  This software is licensed as described in the file LICENSE.txt,
+  which you should have received as part of this distribution.
+  ------------------------------------------------------------------------------*/
 package charlie;
 import labase.poo.ICalculadora;
 
@@ -17,47 +17,48 @@ import labase.poo.ICalculadora;
  * @since   1.0   Esta versao soma um mais um
  */
 public class Calculadora implements ICalculadora {
-  /**Operador da Caculadora. */
-  private Integer operador = new Integer(0);
-  /**Acumulador da Caculadora. */
-  private Integer acumulador = new Integer(0);
+    /**Operador da Caculadora. */
+    private Integer operador = new Integer(0);
+    /**Acumulador da Caculadora. */
+    private Integer acumulador = new Integer(0);
 
-  /**
-   * Construtor para objetos da classe Calculadora.
-   */
-  public Calculadora() {
-    // inicializa variaveis de inst�ncia
-  }
+    /**
+     * Construtor para objetos da classe Calculadora.
+     */
+    public Calculadora() {
+        // inicializa variaveis de instancia
+        operador = acumulador = 0;
+    }
 
-  /**
-   * Entra a tecla um.
-   *
-   * @return  conteudo do operador
-   */
-  public final String entraUm() {
-    operador = operador * 10 + 1;
-    return operador.toString();
-  }
+    /**
+     * Entra a tecla um.
+     *
+     * @return  conteudo do operador
+     */
+    public final String entraUm() {
+        operador = operador * 10 + 1;
+        return operador.toString();
+    }
 
-  /**
-   * Limpa o acumulador.
-   *
-   * @return  conteudo do acumulador
-   */
-  public final String limpa() {
-      acumulador = 0;
-      operador = 0;
-      return operador.toString();
-  }
+    /**
+     * Limpa o acumulador.
+     *
+     * @return  conteudo do acumulador
+     */
+    public final String limpa() {
+        acumulador = 0;
+        operador = 0;
+        return operador.toString();
+    }
 
-  /**
-   * Entra o comando soma.
-   *
-   * @return  conteudo do acumulador
-   */
-  public final String comandoSoma() {
-      acumulador += operador;
-      operador = 0;
-      return acumulador.toString();
-  }
+    /**
+     * Entra o comando soma.
+     *
+     * @return  conteudo do acumulador
+     */
+    public final String comandoSoma() {
+        acumulador += operador;
+        operador = 0;
+        return acumulador.toString();
+    }
 }
