0a443f7fd0d76c63744e2fa9f1867c7fe956e1de
kuarupp <kuarupp@980eeb38-f416-11dd-aa46-671127399dce>
2009-04-07 12:32:25 +0000

    Inseridos javadoc na CalculadoraTest

--- poo09/projetos/trunk/golf/src/main/java/golf/CalculadoraTest.java
+++ poo09/projetos/trunk/golf/src/main/java/golf/CalculadoraTest.java
@@ -8,13 +8,11 @@ package golf;
  * @author  (your name)
  * @version (a version number or a date)
  */
-public class CalculadoraTest extends junit.framework.TestCase
-{
+public class CalculadoraTest extends junit.framework.TestCase{
     /**
      * Default constructor for test class CalculadoraTest
      */
-    public CalculadoraTest()
-    {
+    public CalculadoraTest(){
     }
 
     /**
@@ -22,8 +20,7 @@ public class CalculadoraTest extends junit.framework.TestCase
      *
      * Called before every test case method.
      */
-    protected void setUp()
-    {
+    protected void setUp(){
     }
 
     /**
@@ -31,73 +28,104 @@ public class CalculadoraTest extends junit.framework.TestCase
      *
      * Called after every test case method.
      */
-    protected void tearDown()
-    {
+    protected void tearDown(){
     }
 
-	public void test_digita_um_em_repouso()
-	{
-		golf.Calculadora calculad1 = new golf.Calculadora();
-		calculad1.limpa();
-		assertEquals("1", calculad1.entraUm());
-	}
+    /**
+     * Testa o resultado de digitar um quando esta em repouso.
+     *
+     * @return  1
+     */
+    public void testDigitaUmEmRepouso(){
+        golf.Calculadora calculad1 = new golf.Calculadora();
+        calculad1.limpa();
+        assertEquals("1", calculad1.entraUm());
+    }
+    
+    /**
+     * Testa o resultado de digitar um duas vezes.
+     *
+     * @return  11
+     */
+    public void testDigitaUmDuasVezes(){
+        golf.Calculadora calculad1 = new golf.Calculadora();
+        calculad1.entraUm();
+        assertEquals("11", calculad1.entraUm());
+    }
 
-	public void test_digita_um_duas_vezes()
-	{
-		golf.Calculadora calculad1 = new golf.Calculadora();
-		calculad1.entraUm();
-		assertEquals("11", calculad1.entraUm());
-	}
+    /**
+     * Testa o que sobra no operador apos limpar o seu conteudo.
+     *
+     * @return  0
+     */
+    public void testLimpaConteudo(){
+        golf.Calculadora calculad1 = new golf.Calculadora();
+        calculad1.entraUm();
+        assertEquals("0", calculad1.limpa());
+    }
 
-	public void test_limpa_conteudo()
-	{
-		golf.Calculadora calculad1 = new golf.Calculadora();
-		calculad1.entraUm();
-		assertEquals("0", calculad1.limpa());
-	}
+    /**
+     * Testa o o resultado da soma 1 + 1.
+     *
+     * @return  2
+     */
+    public void testSomaUmMaisUm(){
+        golf.Calculadora calculad1 = new golf.Calculadora();
+        calculad1.entraUm();
+        calculad1.comandoSoma();
+        calculad1.entraUm();
+        assertEquals("2", calculad1.comandoSoma());
+    }
 
-	public void test_soma_um_mais_um()
-	{
-		golf.Calculadora calculad1 = new golf.Calculadora();
-		calculad1.entraUm();
-		calculad1.comandoSoma();
-		calculad1.entraUm();
-		assertEquals("2", calculad1.comandoSoma());
-	}
+    /**
+     * Testa o que acontece se digitar diretamente a tecla de soma em repouso.
+     *
+     * @return  0
+     */
+    public void testDigitaMaisEmRepouso(){
+        golf.Calculadora calculad1 = new golf.Calculadora();
+        assertEquals("0", calculad1.comandoSoma());
+    }
 
-	public void test_digita_mais_em_repouso()
-	{
-		golf.Calculadora calculad1 = new golf.Calculadora();
-		assertEquals("0", calculad1.comandoSoma());
-	}
+    /**
+     * Testa o que acontece se digitar diretamente a tecla de soma e a tecla 1.
+     *
+     * @return  1
+     */
+    public void testDigitaSomaUm(){
+        golf.Calculadora calculad1 = new golf.Calculadora();
+        calculad1.comandoSoma();
+        assertEquals("1", calculad1.entraUm());
+    }
 
-	public void test_digita_soma_um()
-	{
-		golf.Calculadora calculad1 = new golf.Calculadora();
-		calculad1.comandoSoma();
-		assertEquals("1", calculad1.entraUm());
-	}
+    /**
+     * Testa o que acontece se digitar a tecla 1 tres vezes.
+     *
+     * @return  111
+     */
+    public void testDigitaUmTresVezes(){
+        golf.Calculadora calculad1 = new golf.Calculadora();
+        calculad1.entraUm();
+        calculad1.entraUm();
+        assertEquals("111", calculad1.entraUm());
+    }
 
-	public void test_digita_um_tres_vezes()
-	{
-		golf.Calculadora calculad1 = new golf.Calculadora();
-		calculad1.entraUm();
-		calculad1.entraUm();
-		assertEquals("111", calculad1.entraUm());
-	}
-
-	public void test_soma_limpa_soma()
-	{
-		golf.Calculadora calculad1 = new golf.Calculadora();
-		calculad1.entraUm();
-		calculad1.comandoSoma();
-		calculad1.entraUm();
-		calculad1.limpa();
-		calculad1.entraUm();
-		calculad1.comandoSoma();
-		calculad1.entraUm();
-		assertEquals("2", calculad1.comandoSoma());
-	}
+    /**
+     * Testa o que acontece se somar 1 + 1, limpar e somar 1 + 1 novamente.
+     *
+     * @return  2
+     */
+    public void testSomaLimpaSoma(){
+        golf.Calculadora calculad1 = new golf.Calculadora();
+        calculad1.entraUm();
+        calculad1.comandoSoma();
+        calculad1.entraUm();
+        calculad1.limpa();
+        calculad1.entraUm();
+        calculad1.comandoSoma();
+        calculad1.entraUm();
+        assertEquals("2", calculad1.comandoSoma());
+    }
 }
 
 
