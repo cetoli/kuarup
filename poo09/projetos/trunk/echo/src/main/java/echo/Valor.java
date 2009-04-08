3564ba9c4a10c15a30ebd326b69709b1c415ad19
kuarupp <kuarupp@980eeb38-f416-11dd-aa46-671127399dce>
2009-04-08 03:25:56 +0000

    Trabalho 2 - versao com warnings

--- poo09/projetos/trunk/echo/src/main/java/echo/Valor.java
+++ poo09/projetos/trunk/echo/src/main/java/echo/Valor.java
@@ -3,11 +3,11 @@ package echo;
 
 /**
  * Write a description of class Valor here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Valor
+public class Valor implements Cloneable
 {
     protected int valor;
 
@@ -16,30 +16,56 @@ public class Valor
      */
     public Valor()
     {
-        
+
         valor = 0;
     }
-    
+
     public Valor(int novo)
     {
-        
+
         valor = novo;
+    }
+
+    public Object clone () {
+        try {
+            return super.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            throw new InternalError(e.toString());
+        }
+    }
+
+    public String limparValor(){
+        valor= 0;
+
+        return converterEmString ();
     }
 
     public int getValor() {
         return valor;
     }
-    
-    public void adicionarDigito (String digito) {
-    
-        
+
+    public String adicionarDigito (String digito) {
+        String str= converterEmString () + digito;
+
+        valor= Integer.parseInt (str);
+
+        return converterEmString ();
     }
-    
+
     public void setValor(String novo) {
         valor = Integer.parseInt (novo);
     }
-    
+
     public String converterEmString() {
-        return new Integer(valor).toString();
+        return String.valueOf(valor);
     }
+
+    public void somar(Valor objOp2){
+        int op2= objOp2.getValor ();
+
+        valor+= op2;
+
+    }
+
 }
