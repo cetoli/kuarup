4bcbe00a625626687e316ad5047814b417dee8f7
kuarupp <kuarupp@980eeb38-f416-11dd-aa46-671127399dce>
2009-04-11 14:22:46 +0000

    Calculadora - aula 2 - Strategy Teclas um, mais, decimal, binario e hexadecimal.

---
+++ poo09/projetos/tags/charlie/charlie2/main/java/charlie/BaseStrategy.java
@@ -0,0 +1,20 @@
+package charlie;
+
+/**
+ * Interface da estrategia de Base de operacao.
+ * @author  Andre Abrantes  $Author$
+ * @author  Helio Salmon  $Author$
+ * @version 1.0
+ */
+
+public interface BaseStrategy {
+    /**
+     * Esta e a Base de calculo.
+     */    
+    Integer entraUm(Integer integer); 
+    
+    /**
+     * Conversor de inteiro para string.
+     */
+    String toString (Integer integer);
+}
