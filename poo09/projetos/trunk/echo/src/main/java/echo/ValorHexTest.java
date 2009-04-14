84d5501bf0555f19253d64ca2ff3f24d43e49d0b
kuarupp <kuarupp@980eeb38-f416-11dd-aa46-671127399dce>
2009-04-14 01:53:59 +0000

    Trabalho 2: Criação do teste para as clases ValorHex, Valor e ValorBin. Outra alteração foi a remoção de warnings.

---
+++ poo09/projetos/trunk/echo/src/main/java/echo/ValorHexTest.java
@@ -0,0 +1,63 @@
+package echo;
+
+
+
+/**
+ * Classe de teste da classe ValorHex.
+ *
+ * @author  (Marcos de Castro)  $Author$
+ * @author  (Rodrigo Santos Borges)  $Author$
+ * @version 1.0
+ */
+public class ValorHexTest extends junit.framework.TestCase
+{
+    /**
+     * Default constructor for test class ValorHexTest.
+     */
+    public ValorHexTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    protected void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    protected void tearDown()
+    {
+    }
+
+    /**
+     * testa conversão para hexadecimal.
+     */
+	public void testConverterHex()
+	{
+		echo.ValorHex valor1 = new echo.ValorHex(0);
+		valor1.setValor ("2a");
+		assertEquals ("2a", valor1.converterEmString ());
+	}
+
+	/**
+	 * Testa a adição de dígitos.
+	 */
+	public void testAdicionarDigito()
+	{
+		echo.ValorHex valorHex1 = new echo.ValorHex(0);
+		assertEquals("1", valorHex1.adicionarDigito("1"));
+		assertEquals("11", valorHex1.adicionarDigito("1"));
+		assertEquals("111", valorHex1.adicionarDigito("1"));
+		assertEquals("111", valorHex1.converterEmString());
+		assertEquals (273, valorHex1.getValor());
+	}
+}
+
+
