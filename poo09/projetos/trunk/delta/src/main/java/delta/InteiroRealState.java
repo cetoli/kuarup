641e087f746b41df7ce2b4f08c8619c63eb61374
kuarupp <kuarupp@980eeb38-f416-11dd-aa46-671127399dce>
2009-04-28 16:54:58 +0000

    checkstyle

--- poo09/projetos/trunk/delta/src/main/java/delta/InteiroRealState.java
+++ poo09/projetos/trunk/delta/src/main/java/delta/InteiroRealState.java
@@ -18,8 +18,8 @@ public class InteiroRealState implements InteiroState {
     /**
      *
      */
-    public final String toString
-        (final Inteiro inteiro, final BaseStrategy base) {
+    public final String toString(final Inteiro inteiro,
+            final BaseStrategy base) {
         return base.toString(inteiro.getParteReal());
     }
 
@@ -33,8 +33,8 @@ public class InteiroRealState implements InteiroState {
     /**
      *
      */
-    public final void decideNovoEstado
-        (final Inteiro inteiro, final InteiroState inteiroState) {
+    public final void decideNovoEstado(final Inteiro inteiro,
+            final InteiroState inteiroState) {
         inteiro.setEstado(inteiroState);
     }
 }
