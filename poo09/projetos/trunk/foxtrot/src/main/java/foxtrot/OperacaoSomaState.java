ca8773d131331978d7ddadacda5b27962dbb9c31
kuarupp <kuarupp@980eeb38-f416-11dd-aa46-671127399dce>
2009-05-12 13:58:37 +0000

    foxtrot

--- poo09/projetos/trunk/foxtrot/src/main/java/foxtrot/OperacaoSomaState.java
+++ poo09/projetos/trunk/foxtrot/src/main/java/foxtrot/OperacaoSomaState.java
@@ -2,10 +2,17 @@ package foxtrot;
 
 
 /**
- * Write a description of class OperacaoSoma here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Classe que implementa a calculadora.
+ * @author  Carlos Eduardo e Andre de Abrantes
+ * @version 5.0 12/05/2009 Carlos Eduardo e Andre de Abrantes
+ * @since   4.0 Inclu�das as funcionalidades: numeros vetoriais e subtracao
+ * @version 4.0 05/05/2009 Carlos Eduardo e Thiago Silva de Souza
+ * @since   3.0 Inclu�das as funcionalidades: n�meros com nota��o cient�fica.
+ * @version 3.0 28/04/2009  Carlos Eduardo e Rodrigo Borges
+ * @since   2.0 Inclu�das as funcionalidades: n�meros complexos.
+ * @version 2.0 14/04/2009 Carlos Eduardo e Carlos Henrique
+ * @since   1.0 Inclu�das as funcionalidades: n�meros em bases bin�rias e
+ * hexadecimais.
  */
 public class OperacaoSomaState implements OperacaoState {
 
